c4452e5a1239317c1466e911200cefcad0103cba
Vinny Principe <vprincipe@gmail.com>
2019-06-28 23:18:24 -0400

    updated index, nav, and added rest of images

---
+++ clickgame/src/images.js
@@ -0,0 +1,23 @@
+import leela from './images/leela.png';
+import fry from './images/fry.png';
+import bender from './images/bender.png';
+import hermes from './images/hermes.png';
+import kif from './images/kif.png';
+import professor from './images/professor.png';
+import zapp from './images/zapp.png';
+import zoidberg from './images/zoidberg.png';
+
+
+
+const images = [
+    leela,
+    fry,
+    bender,
+    hermes,
+    kif,
+    professor,
+    zapp,
+    zoidberg,
+];
+
+export default images;
